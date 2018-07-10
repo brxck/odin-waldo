09fd17fd900f6dd0e7a22cef0902e02cc833cca5
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-07-10 13:16:06 -0700

    Migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_10_011916) do
+ActiveRecord::Schema.define(version: 2018_07_10_195945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2018_07_10_011916) do
     t.datetime "updated_at", null: false
     t.string "author"
     t.string "website"
+    t.string "title"
   end
 
   add_foreign_key "people", "pictures"
