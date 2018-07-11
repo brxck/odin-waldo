22fba5cfc8a4fa1904ad0091a77e143a14b8a55e
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-07-11 16:51:32 -0700

    Implement front to back end communication

--- app/assets/javascripts/pictures.js
+++ app/assets/javascripts/pictures.js
@@ -4,7 +4,9 @@ window.onload = () => {
 }
 
 const userClick = e => {
+  // alert(e.clientX.toString() + ", " + e.clientY.toString())
   animateClick(e.clientX, e.clientY)
+  searchPerson(e.clientX, e.clientY)
 }
 
 const animateClick = (x, y) => {
@@ -19,4 +21,20 @@ const animateClick = (x, y) => {
 
   setTimeout(() => ripple.classList.add("ripple-full"), 50)
   setTimeout(() => ripple.classList.add("ripple-fade"), 550)
+}
+
+const searchPerson = (x, y) => {
+  const token = document.getElementsByName("csrf-token")[0].content
+  fetch("search", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "X-CSRF-Token": token
+    },
+    body: JSON.stringify({ coord: { x: x, y: y } })
+  })
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(error => console.error(error))
 }
