048fbedc58dead48e3340085b38be5e2839dc8fd
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-07-14 12:13:29 -0700

    Refactor people controller

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,24 +3,27 @@ class PeopleController < ApplicationController
 
   def search
     picture = Picture.find(params[:id])
+
     @person = picture.people.where(
       ":x > x0 and :x < x1 and :y > y0 and :y < y1",
       x: params[:person][:x], y: params[:person][:y]
     ).first
-    found = @person ? true : false
-    remaining = picture.people.where(found: false).map(&:name)
 
-    @response = {
-      found: found,
-      person: {
-        x0: @person.x0,
-        y0: @person.y0,
-        x1: @person.x1,
-        y1: @person.y1,
-        name: @person.name
-      },
-      remaining: remaining
-    }
+    @response = if @person
+                  {
+                    found: true,
+                    person: {
+                      x0: @person.x0,
+                      y0: @person.y0,
+                      x1: @person.x1,
+                      y1: @person.y1,
+                      name: @person.name
+                    },
+                    remaining: picture.people.where(found: false).map(&:name)
+                  }
+                else
+                  { found: false }
+                end
 
     respond_to do |format|
       format.json { render json: @response }
